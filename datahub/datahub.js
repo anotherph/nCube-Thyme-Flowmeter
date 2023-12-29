bd16af2c6ce52144bcc0e70cbc548e0dc031705d
jekim <anotherph@gmail.com>
2023-12-29 15:28:44 +0900

    add HTTP API

--- datahub/datahub.js
+++ datahub/datahub.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const { get } = require('http');
 const mqtt = require('mqtt');
+const axios = require('axios');
 
 // conf
 conf = require('../conf.js');
@@ -17,6 +18,10 @@ let mobiusTopic = {
 let topicSubscribed = false;
 let requestBodyJson = {};
 let retriData = {};
+
+var mFlowrate = {};
+var mSpeed = {};
+var curTime= {};
 
 let loadEntity = () => {
     console.log('Load entity of waterflow data model...')
@@ -41,16 +46,57 @@ function waitForTopic(callback) {
   });
 }
 
-function setResource() {
-    /* user code */
-    getData();
-    setEntity();
-}
+// let setResource = (callback) => {
+//   waitForTopic(() => {
+//     getData((error, data) => {
+//       if (error) {
+//         callback(error, null); // Pass the error to the outer callback
+//       } else {
+//         setEntity(data);
+//         // Pass the data to postData, and provide a callback for postData
+//         postData(data, (postError, result) => {
+//           if (postError) {
+//             callback(postError, null); // Pass the error to the outer callback
+//           } else {
+//             callback(null, result); // Call the outer callback with result
+//           }
+//         });
+//       }
+//     });
+//   });
+// };
+
+let setResource = (callback) => {
+  waitForTopic(() => {
+    getData((error, data) => {
+      if (error) {
+        callback(error, null);
+      } else {
+        // Get the updated data from setEntity
+        const updatedData = setEntity(data);
+        postData(updatedData, (postError, result) => {
+          if (postError) {
+            callback(postError, null);
+          } else {
+            callback(null, result);
+          }
+        });
+      }
+    });
+  });
+};
 
 client.on('message', (topic, message) => {
   // Check if the subscribed topic is received
   if (topic === mobiusTopic.mobius && topicSubscribed) {
-    setResource();
+    // setResource();
+    setResource((error, result) => {
+      if (error) {
+        console.error(error);
+      } else {
+        console.log(result);
+      }
+    });
   }
 });
 
@@ -62,47 +108,96 @@ client.on('connect', () => {
   });
 });
 
-let getData = () => {
-    const axios = require('axios');
-    let config = {
-    method: 'get',
-    maxBodyLength: Infinity,
-    url: 'http://172.20.0.109:7579/Mobius/KETI_Flowmeter/flowmeter/la', // add Mobius IP to conf.js
-    headers: { 
-        'Accept': 'application/json', 
-        'X-M2M-RI': '12345', 
-        'X-M2M-Origin': 'SKETI_Flowmeter'
-    }
-    };
-    axios.request(config)
-    .then((response) => {
-    // console.log(JSON.stringify(response.data));
-    retriData=JSON.stringify(response.data)
-    // console.log(retriData)
+let getData = (callback) => {
+  console.log('order: 1');
+  let config = {
+      method: 'get',
+      maxBodyLength: Infinity,
+      url: 'http://172.20.0.109:7579/Mobius/KETI_Flowmeter/flowmeter/la', // add mobius IP to conf.js
+      headers: {
+          'Accept': 'application/json',
+          'X-M2M-RI': '12345',
+          'X-M2M-Origin': 'SKETI_Flowmeter'
+      }
+  };
+  axios.request(config)
+      .then((response) => {
+          retriData = JSON.stringify(response.data);
+          // console.log(retriData);
+          callback(null, retriData); // Call the callback with data (no error)
+      })
+      .catch((error) => {
+          console.log(error);
+          callback(error, null); // Call the callback with an error
+      });
+};
+
+let setEntity = (retriData) => {
+
+  console.log('order: 2');
+  
+  //set entity
+  mFlowrate=JSON.parse(retriData)["m2m:cin"]["con"]["m_flowrate"];
+  var matches = mFlowrate.match(/\d+\.\d+/);
+  mFlowrate = matches && matches[0];
+  
+  mSpeed=JSON.parse(retriData)["m2m:cin"]["con"]["m_speed"];
+  matches = mSpeed.match(/\d+\.\d+/);
+  mSpeed = matches && matches[0];
+
+  curTime=JSON.parse(retriData)["m2m:cin"]["con"]["cur_time"];
+  curTime='20' + curTime.replace(/\s/g, 'T') + 'Z';
+
+  // set json file
+  requestBodyJson['entities'][0]['flowRate']['value']=mFlowrate
+  requestBodyJson['entities'][0]['flowRate']['observedAt']=curTime
+  requestBodyJson['entities'][0]['velocity']['value']=mSpeed
+  requestBodyJson['entities'][0]['velocity']['observedAt']=curTime
+
+  // console.log(requestBodyJson)
+
+  let updatedRequestBodyJson = {
+    ...requestBodyJson,
+    entities: [
+      {
+        ...requestBodyJson.entities[0],
+        flowRate: {
+          ...requestBodyJson.entities[0].flowRate,
+          value: mFlowrate,
+          observedAt: curTime,
+        },
+        velocity: {
+          ...requestBodyJson.entities[0].velocity,
+          value: mSpeed,
+          observedAt: curTime,
+        },
+      },
+    ],
+  };
+
+  return updatedRequestBodyJson;  // Return the updated data
+  
+};
+
+let postData = (requestBodyJson, callback) => { // Add callback parameter
+  console.log('order: 3');
+
+  // const apiURL = 'http://203.253.128.181:11003/entityOperations/upsert'; // add mobius IP to conf.js
+  const apiURL = 'http://172.20.0.168:8081/entityOperations/upsert';
+  const requestHeaders = {
+    'Content-Type': 'application/json'
+  };
+
+  console.log(requestBodyJson)
+
+  axios.post(apiURL, requestBodyJson, { headers: requestHeaders })
+    .then(response => {
+      // console.log(response.status);
+      // console.log(response.data);
+      callback(null, response.data); // Call the callback with result
     })
-    .catch((error) => {
-    console.log(error);
+    .catch(error => {
+      // console.error(error);
+      callback(error, null); // Call the callback with an error
     });
-}
-
-let setEntity = () => {
-    // const parsedData = JSON.parse(retriData);
-    const conData = retriData["con"];
-    console.log(typeof retriData)
-
-    // // flowrate
-    // const mFlowrate = conData.m_flowrate
-    // matches = mFlowrate.match(/\d+\.\d+/);
-    // mFlowrate=matches && matches[0];
-    // console.log(mFlowrate)
-    
-    // // speed of water
-    // const mSpeed = conData.m_speed
-    // matches = mSpeed.match(/\d+\.\d+/);
-    // mSpeed=matches && matches[0];
-    // console.log(mSpeed)
-    
-    // current time
-    // const curTime = conData.cur_time
-
 }
